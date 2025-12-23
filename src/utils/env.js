142166cc29446a0170da292de7c1cc34c7d9ea80
DongaDev <101142627+Dongadev17@users.noreply.github.com>
2025-12-23 19:10:16 +0100

    Delete src/utils/env.js

--- src/utils/env.js
+++
@@ -1,6 +0,0 @@
-const OYYI_ENDPOINT = "https://oyyi.xyz/api/image/color-palette";
-const THECOLORAPI_ENDPOINT = "https://www.thecolorapi.com";
-const MAIN_REPO = "Dongadev17/Tintora";
-const GITHUB_RAW_BASE = "https://raw.githubusercontent.com";
-const RAW_GITHUB = `${GITHUB_RAW_BASE}/${MAIN_REPO}/main`;
-const ADS_DATA_URL = `${RAW_GITHUB}/ads.json`;
